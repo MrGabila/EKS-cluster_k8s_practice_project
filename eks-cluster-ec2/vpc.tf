a97700c5399cfb30d44bf5903b60a1b805bf0c35
TheModeler99 <nyuykimo@gmail.com>
2023-11-01 21:22:02 -0400

    Revert "Merge branch 'main' of https://github.com/TheModeler99/EKS-cluster_k8s_practice_project"
    This reverts commit 8d3f7bdcd7bd7aec22bc8119652a9e19891a4db8, reversing
    changes made to ed02f170a55e29b26fae38f48fe73ba6a75582b6.
    
    reverting back to a previous version

--- eks-cluster-ec2/vpc.tf
+++ eks-cluster-ec2/vpc.tf
@@ -96,6 +96,7 @@ resource "aws_eip" "nat" {
 }
 
 # Create a NAT gateway
+
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public-us-east-1a.id
--- eks-cluster-ec2/worker-nodes.tf
+++ eks-cluster-ec2/worker-nodes.tf
@@ -39,6 +39,7 @@ resource "aws_iam_role_policy_attachment" "AmazonElasticFileSystemReadOnlyAccess
 
 
 ## Configure worker nodes creation 
+
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.cluster_name}-node_group"
@@ -54,6 +55,9 @@ resource "aws_eks_node_group" "eks_node_group" {
 
   instance_types = ["t2.medium"]
 
-  depends_on = [ aws_eks_cluster.cluster]
+  depends_on = [
+    aws_eks_cluster.cluster
+  ]
+
 
 }
